d2b95e71d6c0146fb6f9903de7f1f78de21b3610
Auridebson Lima <auridebson@vutus.com>
2023-10-20 12:21:38 -0300

    Update

--- main.py
+++ main.py
@@ -3,3 +3,14 @@ def ln(x):
     print('-'*x)
 
 
+def pegaDados():
+    nome = input("Digite seu nome: ")
+    idade = input("Qual sua idade: ")
+    cidade = input("Que cidade nasceu: ")
+
+    resumo = f'Seu nome é {nome}, nasceu em {cidade} e tem {idade} anos de idade.'
+
+    return print(resumo)
+
+pegaDados()
+ln(60)
